03d2aa3b403b1799d60886b73ee8fa54b6abed5a
Oguzhan Soyer <soyeroguzhan@gmail.com>
2021-05-19 23:09:44 +0200

    finished 1.0 version

--- src/index.js
+++ src/index.js
@@ -14,35 +14,156 @@ class TodoList{
 }
 
 class Project{
-    constructor(name){
+    constructor(name,id){
         this._name = name
+        this.id = id
+        this.tasks = []
     }
+
+    addTask(Task) {
+        this.tasks.push(Task)
+    }
+
+
+    get name(){
+        return this._name
+    }
+}
+
+class Task{
+    constructor(name, id, projectid){
+        this._name = name
+        this.id = id
+        this.projectid = id
+    }
+
     get name(){
         return this._name
     }
 }
 
 
-const myList1 = new TodoList("myList")
-const myProject1 = new Project("myProject")
-myList1.addProject(myProject1);
+const objectManager = (() => {
+    const myList = new TodoList("myList") // create a default list
+    const myProject = new Project("myProject",0) //create a default project
+    myList.addProject(myProject);
+    return {myProject, myList}
+})()
 
-   
-    
+
+
 const loadHomepage = (() => {
 
-    const loadSidebar = (() => {
-        let sidebar = document.getElementById('sidebar')
+    const loadSidebar = () => {
+
+        let sidebar = document.getElementById('projectwrapper')
+        // List all projects of "myList" inside the sidebar 
+        objectManager.myList.projects.forEach((e) => {
+            let project = document.createElement('div')
+            project.classList.add('project')
+            project.setAttribute("data-index",e.id)
+            project.innerHTML = e._name
+            sidebar.appendChild(project)
+            
+            // add eventlistener to clicked project to switch classes and load tasks 
+            project.addEventListener("click", function(e) {
+                let projects = document.getElementsByClassName('project')
+                
+                for (let i = 0; i<projects.length; i++) {
+                    projects[i].className = 'project'
+                }
+
+                project.className = 'project active'
+
+                loadProjectDetail()
+            
+            })
 
 
-    })()
+        });
+    }
+    
+    // call loadsidebar function to show myProject as default 
+    loadSidebar()
 
-    const loadProjectDetail = (() => {
-        console.log("Test B")
-    })()
+
+    const loadProjectDetail = () => {
+        document.getElementById('taskwrapper').innerHTML=''
+
+        try  { 
+            activeproject = document.getElementsByClassName('project active')[0].dataset.index
+            console.log(objectManager.myList.projects[activeproject])
+        }
+        catch {
+            activeproject  = 0
+        }
+        
+
+        let taskwrapper = document.getElementById('taskwrapper')
+        // List all tasks of of the active project
+        objectManager.myList.projects[activeproject].tasks.forEach((e) => {
+            let task = document.createElement('div')
+            task.classList.add('task')
+            task.innerHTML = e._name
+            taskwrapper.appendChild(task)
+        })
+
+        //  list all tasks for activeproject
+
+
+    }
+
+    return {loadSidebar, loadProjectDetail}
 
 })()
 
+
+const submitProject = (() => {
+
+        const form = document.forms[0];
+        // take input value for form and add it to myList
+        
+        form.addEventListener("submit", function(event) {
+
+            event.preventDefault();
+            const projectname = document.getElementById("projectname").value
+            const projectid = objectManager.myList.projects.length 
+            let newproject = new Project(projectname,projectid);
+            objectManager.myList.addProject(newproject);
+            document.getElementById('projectwrapper').innerHTML=''
+            loadHomepage.loadSidebar()
+            document.getElementById('projectname').value = ''    
+
+            });
+})()
+
+const submitTask = (() => {
+
+    const form = document.forms[1];
+    // take input value for form and add it to active Project
+    
+    form.addEventListener("submit", function(event) {
+
+        event.preventDefault();
+        const taskname = document.getElementById("taskname").value
+        const taskid = objectManager.myList.projects.length
+        
+        try  { 
+            projectid = document.getElementsByClassName('project active')[0].dataset.index
+        }
+        catch {
+            projectid = 0
+        }
+        
+        let newtask = new Task(taskname,taskid,projectid);
+        objectManager.myList.projects[projectid].addTask(newtask) //addtask to active project
+        loadHomepage.loadProjectDetail()
+        document.getElementById('taskname').value = ''  
+
+        });
+})()
+
+console.log(objectManager.myList)
 
 
 /* 
@@ -53,9 +174,11 @@ const loadHomepage = (() => {
 
 - js
 -- loadHomepage
--- todolist
--- class Project
---- createProject module (constructor)
+--- loadsidebar (for each entry in myList create a project)
+--- load addProject form
+
+--- onclick auf project (for each entry in {project})
+
 
 -- deleteProject module
 -- createTask module
@@ -65,5 +188,11 @@ const loadHomepage = (() => {
 -- openProjects module
 
 -- localStorage
+
+
+
+- create a function that creates projects and assigns them to do myList
+- create 
+
 
 */
