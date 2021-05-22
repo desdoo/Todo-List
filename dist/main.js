31589a0408025b5b0dc09d47b578d20dfac95f1e
Oguzhan Soyer <soyeroguzhan@gmail.com>
2021-05-22 17:49:53 +0200

    prettier

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,97 @@
-(()=>{class e{constructor(e){this.name=e,this.projects=[]}addProject(e){this.projects.push(e)}get listProjects(){return this.projects}}class t{constructor(e,t){this._name=e,this.id=t,this.tasks=[]}addTask(e){this.tasks.push(e)}get name(){return this._name}}class s{constructor(e,t,s){this._name=e,this.id=t,this.projectid=t}get name(){return this._name}}const c=(()=>{const s=new e("myList"),c=new t("myProject",0);return s.addProject(c),{myProject:c,myList:s}})(),n=(()=>{const e=()=>{let e=document.getElementById("projectwrapper");c.myList.projects.forEach((s=>{let c=document.createElement("div");c.classList.add("project"),c.setAttribute("data-index",s.id),c.innerHTML=s._name,e.appendChild(c),c.addEventListener("click",(function(e){let s=document.getElementsByClassName("project");for(let e=0;e<s.length;e++)s[e].className="project";c.className="project active",t()}))}))};e();const t=()=>{document.getElementById("taskwrapper").innerHTML="";try{activeproject=document.getElementsByClassName("project active")[0].dataset.index,console.log(c.myList.projects[activeproject])}catch{activeproject=0}let e=document.getElementById("taskwrapper");c.myList.projects[activeproject].tasks.forEach((t=>{let s=document.createElement("div");s.classList.add("task"),s.innerHTML=t._name,e.appendChild(s)}))};return{loadSidebar:e,loadProjectDetail:t}})();document.forms[0].addEventListener("submit",(function(e){e.preventDefault();const s=document.getElementById("projectname").value,a=c.myList.projects.length;let o=new t(s,a);c.myList.addProject(o),document.getElementById("projectwrapper").innerHTML="",n.loadSidebar(),document.getElementById("projectname").value=""})),document.forms[1].addEventListener("submit",(function(e){e.preventDefault();const t=document.getElementById("taskname").value,a=c.myList.projects.length;try{projectid=document.getElementsByClassName("project active")[0].dataset.index}catch{projectid=0}let o=new s(t,a,projectid);c.myList.projects[projectid].addTask(o),n.loadProjectDetail(),document.getElementById("taskname").value=""})),console.log(c.myList)})();
+(() => {
+  class e {
+    constructor(e) {
+      (this.name = e), (this.projects = []);
+    }
+    addProject(e) {
+      this.projects.push(e);
+    }
+    get listProjects() {
+      return this.projects;
+    }
+  }
+  class t {
+    constructor(e, t) {
+      (this._name = e), (this.id = t), (this.tasks = []);
+    }
+    addTask(e) {
+      this.tasks.push(e);
+    }
+    get name() {
+      return this._name;
+    }
+  }
+  class s {
+    constructor(e, t, s) {
+      (this._name = e), (this.id = t), (this.projectid = t);
+    }
+    get name() {
+      return this._name;
+    }
+  }
+  const c = (() => {
+      const s = new e("myList"),
+        c = new t("myProject", 0);
+      return s.addProject(c), { myProject: c, myList: s };
+    })(),
+    n = (() => {
+      const e = () => {
+        let e = document.getElementById("projectwrapper");
+        c.myList.projects.forEach((s) => {
+          let c = document.createElement("div");
+          c.classList.add("project"),
+            c.setAttribute("data-index", s.id),
+            (c.innerHTML = s._name),
+            e.appendChild(c),
+            c.addEventListener("click", function (e) {
+              let s = document.getElementsByClassName("project");
+              for (let e = 0; e < s.length; e++) s[e].className = "project";
+              (c.className = "project active"), t();
+            });
+        });
+      };
+      e();
+      const t = () => {
+        document.getElementById("taskwrapper").innerHTML = "";
+        try {
+          (activeproject =
+            document.getElementsByClassName("project active")[0].dataset.index),
+            console.log(c.myList.projects[activeproject]);
+        } catch {
+          activeproject = 0;
+        }
+        let e = document.getElementById("taskwrapper");
+        c.myList.projects[activeproject].tasks.forEach((t) => {
+          let s = document.createElement("div");
+          s.classList.add("task"), (s.innerHTML = t._name), e.appendChild(s);
+        });
+      };
+      return { loadSidebar: e, loadProjectDetail: t };
+    })();
+  document.forms[0].addEventListener("submit", function (e) {
+    e.preventDefault();
+    const s = document.getElementById("projectname").value,
+      a = c.myList.projects.length;
+    let o = new t(s, a);
+    c.myList.addProject(o),
+      (document.getElementById("projectwrapper").innerHTML = ""),
+      n.loadSidebar(),
+      (document.getElementById("projectname").value = "");
+  }),
+    document.forms[1].addEventListener("submit", function (e) {
+      e.preventDefault();
+      const t = document.getElementById("taskname").value,
+        a = c.myList.projects.length;
+      try {
+        projectid =
+          document.getElementsByClassName("project active")[0].dataset.index;
+      } catch {
+        projectid = 0;
+      }
+      let o = new s(t, a, projectid);
+      c.myList.projects[projectid].addTask(o),
+        n.loadProjectDetail(),
+        (document.getElementById("taskname").value = "");
+    }),
+    console.log(c.myList);
+})();
--- src/index.js
+++ src/index.js
@@ -1,170 +1,148 @@
-class TodoList{
-    constructor (name) {
-        this.name = name
-        this.projects = []
-    }
+class TodoList {
+  constructor(name) {
+    this.name = name;
+    this.projects = [];
+  }
 
-    addProject(Project) {
-    this.projects.push(Project)
-    }
+  addProject(Project) {
+    this.projects.push(Project);
+  }
 
-    get listProjects() {
-        return this.projects
-    }
+  get listProjects() {
+    return this.projects;
+  }
 }
 
-class Project{
-    constructor(name,id){
-        this._name = name
-        this.id = id
-        this.tasks = []
-    }
+class Project {
+  constructor(name, id) {
+    this._name = name;
+    this.id = id;
+    this.tasks = [];
+  }
 
-    addTask(Task) {
-        this.tasks.push(Task)
-    }
+  addTask(Task) {
+    this.tasks.push(Task);
+  }
 
-
-    get name(){
-        return this._name
-    }
+  get name() {
+    return this._name;
+  }
 }
 
-class Task{
-    constructor(name, id, projectid){
-        this._name = name
-        this.id = id
-        this.projectid = id
-    }
+class Task {
+  constructor(name, id, projectid) {
+    this._name = name;
+    this.id = id;
+    this.projectid = id;
+  }
 
-    get name(){
-        return this._name
-    }
+  get name() {
+    return this._name;
+  }
 }
-
 
 const objectManager = (() => {
-    const myList = new TodoList("myList") // create a default list
-    const myProject = new Project("myProject",0) //create a default project
-    myList.addProject(myProject);
-    return {myProject, myList}
-})()
-
-
+  const myList = new TodoList("myList"); // create a default list
+  const myProject = new Project("myProject", 0); //create a default project
+  myList.addProject(myProject);
+  return { myProject, myList };
+})();
 
 const loadHomepage = (() => {
+  const loadSidebar = () => {
+    let sidebar = document.getElementById("projectwrapper");
+    // List all projects of "myList" inside the sidebar
+    objectManager.myList.projects.forEach((e) => {
+      let project = document.createElement("div");
+      project.classList.add("project");
+      project.setAttribute("data-index", e.id);
+      project.innerHTML = e._name;
+      sidebar.appendChild(project);
 
-    const loadSidebar = () => {
+      // add eventlistener to clicked project to switch classes and load tasks
+      project.addEventListener("click", function (e) {
+        let projects = document.getElementsByClassName("project");
 
-        let sidebar = document.getElementById('projectwrapper')
-        // List all projects of "myList" inside the sidebar 
-        objectManager.myList.projects.forEach((e) => {
-            let project = document.createElement('div')
-            project.classList.add('project')
-            project.setAttribute("data-index",e.id)
-            project.innerHTML = e._name
-            sidebar.appendChild(project)
-            
-            // add eventlistener to clicked project to switch classes and load tasks 
-            project.addEventListener("click", function(e) {
-                let projects = document.getElementsByClassName('project')
-                
-                for (let i = 0; i<projects.length; i++) {
-                    projects[i].className = 'project'
-                }
-
-                project.className = 'project active'
-
-                loadProjectDetail()
-            
-            })
-
-
-        });
-    }
-    
-    // call loadsidebar function to show myProject as default 
-    loadSidebar()
-
-
-    const loadProjectDetail = () => {
-        document.getElementById('taskwrapper').innerHTML=''
-
-        try  { 
-            activeproject = document.getElementsByClassName('project active')[0].dataset.index
-            console.log(objectManager.myList.projects[activeproject])
+        for (let i = 0; i < projects.length; i++) {
+          projects[i].className = "project";
         }
-        catch {
-            activeproject  = 0
-        }
-        
 
-        let taskwrapper = document.getElementById('taskwrapper')
-        // List all tasks of of the active project
-        objectManager.myList.projects[activeproject].tasks.forEach((e) => {
-            let task = document.createElement('div')
-            task.classList.add('task')
-            task.innerHTML = e._name
-            taskwrapper.appendChild(task)
-        })
+        project.className = "project active";
 
-        //  list all tasks for activeproject
+        loadProjectDetail();
+      });
+    });
+  };
 
+  // call loadsidebar function to show myProject as default
+  loadSidebar();
 
+  const loadProjectDetail = () => {
+    document.getElementById("taskwrapper").innerHTML = "";
+
+    try {
+      activeproject =
+        document.getElementsByClassName("project active")[0].dataset.index;
+      console.log(objectManager.myList.projects[activeproject]);
+    } catch {
+      activeproject = 0;
     }
 
-    return {loadSidebar, loadProjectDetail}
+    let taskwrapper = document.getElementById("taskwrapper");
+    // List all tasks of of the active project
+    objectManager.myList.projects[activeproject].tasks.forEach((e) => {
+      let task = document.createElement("div");
+      task.classList.add("task");
+      task.innerHTML = e._name;
+      taskwrapper.appendChild(task);
+    });
 
-})()
+    //  list all tasks for activeproject
+  };
 
+  return { loadSidebar, loadProjectDetail };
+})();
 
 const submitProject = (() => {
+  const form = document.forms[0];
+  // take input value for form and add it to myList
 
-        const form = document.forms[0];
-        // take input value for form and add it to myList
-        
-        form.addEventListener("submit", function(event) {
-
-            event.preventDefault();
-            const projectname = document.getElementById("projectname").value
-            const projectid = objectManager.myList.projects.length 
-            let newproject = new Project(projectname,projectid);
-            objectManager.myList.addProject(newproject);
-            document.getElementById('projectwrapper').innerHTML=''
-            loadHomepage.loadSidebar()
-            document.getElementById('projectname').value = ''    
-
-            });
-})()
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const projectname = document.getElementById("projectname").value;
+    const projectid = objectManager.myList.projects.length;
+    let newproject = new Project(projectname, projectid);
+    objectManager.myList.addProject(newproject);
+    document.getElementById("projectwrapper").innerHTML = "";
+    loadHomepage.loadSidebar();
+    document.getElementById("projectname").value = "";
+  });
+})();
 
 const submitTask = (() => {
+  const form = document.forms[1];
+  // take input value for form and add it to active Project
 
-    const form = document.forms[1];
-    // take input value for form and add it to active Project
-    
-    form.addEventListener("submit", function(event) {
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const taskname = document.getElementById("taskname").value;
+    const taskid = objectManager.myList.projects.length;
 
-        event.preventDefault();
-        const taskname = document.getElementById("taskname").value
-        const taskid = objectManager.myList.projects.length
-        
-        try  { 
-            projectid = document.getElementsByClassName('project active')[0].dataset.index
-        }
-        catch {
-            projectid = 0
-        }
-        
-        let newtask = new Task(taskname,taskid,projectid);
-        objectManager.myList.projects[projectid].addTask(newtask) //addtask to active project
-        loadHomepage.loadProjectDetail()
-        document.getElementById('taskname').value = ''  
+    try {
+      projectid =
+        document.getElementsByClassName("project active")[0].dataset.index;
+    } catch {
+      projectid = 0;
+    }
 
-        });
-})()
+    let newtask = new Task(taskname, taskid, projectid);
+    objectManager.myList.projects[projectid].addTask(newtask); //addtask to active project
+    loadHomepage.loadProjectDetail();
+    document.getElementById("taskname").value = "";
+  });
+})();
 
-console.log(objectManager.myList)
-
+console.log(objectManager.myList);
 
 /* 
 - html structure:
